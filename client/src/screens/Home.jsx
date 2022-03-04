40e0923169f3761ee7adcc7f05d1d1df9b21835e
yash3456789 <79311343+yash3456789@users.noreply.github.com>
2022-03-04 16:38:58 +0530

    Happy halfway 04/03

--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import DishCard2 from "../components/home/DishCard2";
+import Happy from "../components/home/Happy";
+// import DishCard2 from "../components/home/DishCard2";
 // import RasoiCard from "../components/home/RasoiCard";
 
 const Home = () => {
   return (
     <>
       {/* <RasoiCard /> */}
-      <DishCard2 />
+      {/* <DishCard2 /> */}
+      <Happy/>
     </>
   );
 };
